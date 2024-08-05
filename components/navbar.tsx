9c7d6c379abfa83cedec6c26ffed05a4c26e03e1
Utkarsh Krishna <itsutkarshk711@gmail.com>
2024-08-05 23:35:59 +0530

    updated navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 import { Righteous } from "next/font/google";
 import Link from "next/link";
 import style from "@/styles/navbar.module.css";
-import { KeyboardEventHandler, useState } from "react";
+import BrandLogo from "./brand";
 
 const anton = Righteous({
 	weight: ["400"],
@@ -11,12 +11,7 @@ const anton = Righteous({
 export default function Navbar() {
 	return (
 		<div className={style.navbar}>
-			<Link
-				href="/"
-				className={anton.className + " " + style.brandlogo}
-			>
-				PRICES.CO
-			</Link>
+			<BrandLogo />
 			<ul className={style.navlist}>
 				<li className={style.navlink}>TRACKER</li>
 				<li className={style.navlink}>ABOUT</li>
