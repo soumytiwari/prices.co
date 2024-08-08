d354a6cd08fcb8cfb82295d4b56ee14cdd5b18ee
Utkarsh Krishna <itsutkarshk711@gmail.com>
2024-08-08 23:00:24 +0530

    updated products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import Ad from "@/components/products/ad";
 import SearchBar from "@/components/products/search";
 import style from "@/styles/productPage.module.css";
 export default function ProductPage() {
 	return (
 		<div className={style.productPage}>
 			<SearchBar />
+			<Ad/>
 		</div>
 	);
 }
