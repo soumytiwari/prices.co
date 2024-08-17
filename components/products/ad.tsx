97f7f4bd8a850b5adf25705de85459b49e6931af
Utkarsh Krishna <itsutkarshk711@gmail.com>
2024-08-17 22:13:45 +0530

    modified ad component

--- components/products/ad.tsx
+++ components/products/ad.tsx
@@ -1,37 +1,27 @@
-import styles from '@/styles/ads.module.css'
+import styles from '@/styles/ads.module.css';
 import { useEffect, useState } from 'react';
 
-const adCards = [
-    
-    {
-    id: '1',
-    imageURL : '/images/ads/cloth.jpeg',
-    content: ''},
-    {
-    id: '2',
-    imageURL : '/images/ads/sale.jpeg',
-    content: ''}
-
-]
 export default function Ad() {
     const [currentSlide, setCurrentSlide] = useState(0);
     const slides = [
-        { id: 1, imgURL: '/images/ads/cloth.jpeg', text: 'Slide 1' },
-        { id: 2, imgURL: '/images/ads/sale.jpeg', text: 'Slide 2' },
-        { id: 3, imgURL: '/images/ads/e.jpg', text: 'Slide 3' },
-        { id: 4, imgURL: '/images/ads/h.jpg', text: 'Slide 4' },
-        { id: 5, imgURL: '/images/ads/cloth2.jpg', text: 'Slide 4' },
+        { id: 1, imgURL: '/images/ads/cloth.jpeg', color:'#4551b8db', text: 'Slide 1' },
+        { id: 2, imgURL: '/images/ads/sale.jpeg',  color:'#ff0000db', text: 'Slide 2' },
+        { id: 3, imgURL: '/images/ads/e.jpg',      color:'#ffffffdb', text: 'Slide 3' },
+        { id: 4, imgURL: '/images/ads/apple.jpg',  color:'#f0f9a0db', text: 'Slide 4' },
+        { id: 5, imgURL: '/images/ads/glass.jpg',  color:'#ffffffdb', text: 'Slide 5' },
+        { id: 6, imgURL: '/images/ads/wander.jpg', color:'#ffffffdb', text: 'Slide 6' },
+        { id: 7, imgURL: '/images/ads/c.jpg',      color:'#f797e2db', text: 'Slide 7' },
     ];
     const totalSlides = slides.length;
 
     const nextSlide = () => {
-        setCurrentSlide((prev) => (prev+1) % totalSlides);
+        setCurrentSlide((prev) => (prev + 1) % totalSlides);
     };
 
     useEffect(() => {
         const interval = setInterval(nextSlide, 3000); // Change slide every 3 seconds
         return () => clearInterval(interval); // Clean up interval on component unmount
-    }, []);
+    }, [currentSlide]);
 
     return (
         <div className={styles.slider}>
@@ -43,12 +33,17 @@ export default function Ad() {
                     <div
                         key={slide.id}
                         className={styles.slide}
-                        style={{ backgroundImage: `url(${slide.imgURL })`}}
+                        style={{ backgroundImage: `url(${slide.imgURL})` }}
                     >
-                        <div className={styles.content}>
-                            <span>#Big Fashion Sale</span>
+                        <div className={styles.content} style={{background: `linear-gradient(to right,${slide.color} 10%, rgb(0,0,0,0))`}}>
+                            <span style={{marginBottom:'40px'}}>#Big Fashion Sale</span>
                             <h1 className={styles.bold}>Limited time offer!</h1>
-                            <h1 className={styles.bold}>Save up to <span className={styles.bold} style={{color:'red'}}>50%</span></h1>
+                            <h1 className={styles.bold}>
+                                Save up to{' '}
+                                <span className={styles.bold} style={{ color: 'white' }}>
+                                    50%
+                                </span>
+                            </h1>
                             <button className={styles.btn}></button>
                         </div>
                     </div>
@@ -67,4 +62,4 @@ export default function Ad() {
             </div>
         </div>
     );
-};
+}
