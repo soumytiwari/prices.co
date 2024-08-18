2f04963cdafece8f48c1fb1588a797fe1df279cd
Utkarsh Krishna <itsutkarshk711@gmail.com>
2024-08-18 16:25:55 +0530

    modified homepage

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Ad from "@/components/products/ad";
 import SearchBar from "@/components/products/search";
+import ResultProducts from "@/components/products/searchResult";
 import style from "@/styles/productPage.module.css";
 export default function ProductPage() {
 	return (
@@ -8,7 +9,7 @@ export default function ProductPage() {
 			<SearchBar />
 			{/* TODO : add path here eg. Home >> products >> ... >> */}
 			<Ad/>
-			
+			<ResultProducts />
 		</div>
 	);
 }
