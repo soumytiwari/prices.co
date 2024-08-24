45be51fad1ab37177a05f58eca44155cd771a4a4
Utkarsh Krishna <itsutkarshk711@gmail.com>
2024-08-24 17:54:39 +0530

    modifiedcomponent

--- components/products/searchResult.tsx
+++ components/products/searchResult.tsx
@@ -1,121 +1,136 @@
 import styles from '@/styles/searchResults.module.css'
 import Cards from '../productCards';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import FilterMenu from './filters';
+import { MyContext } from './context';
 
-let products = [
+interface product {
+		id: string;
+		url: string;
+		title: string;
+		price: string;
+		rating: number;
+}
+let products : product[] = [
 	{
 		id: "1",
 		url: "",
 		title: "phone lorem epsum dolor sit",
-		price: "$2000",
+		price: "$99",
 		rating: 4.5,
 	},
 	{
 		id: "2",
 		url: "",
 		title: "watch tres do unus",
-		price: "$2000",
+		price: "$1299",
 		rating: 4,
 	},
-	{ id: "3", url: "", title: "nihil nihil nihil", price: "$2000", rating: 3 },
+	{ id: "3", url: "", title: "nihil nihil nihil", price: "$348", rating: 3 },
 	{
 		id: "4",
 		url: "",
 		title: "blade of miquella , malenaia",
-		price: "$2000",
+		price: "$449",
 		rating: 3.7,
 	},
 	{
 		id: "5",
 		url: "",
 		title: "starscourage rhadhan",
-		price: "$2000",
+		price: "$299",
 		rating: 4.4,
 	},
 	{
 		id: "6",
 		url: "",
 		title: "phone lorem epsum dolor sit",
-		price: "$2000",
+		price: "$499",
 		rating: 4.5,
 	},
 	{
 		id: "7",
 		url: "",
 		title: "watch tres do unus",
-		price: "$2000",
+		price: "$799",
 		rating: 4,
 	},
-	{ id: "8", url: "", title: "nihil nihil nihil", price: "$2000", rating: 3 },
+	{ id: "8", url: "", title: "nihil nihil nihil", price: "$1199", rating: 3 },
 	{
 		id: "9",
 		url: "",
 		title: "blade of miquella , malenaia",
-		price: "$2000",
+		price: "$499",
 		rating: 3.7,
 	},
 	{
 		id: "10",
 		url: "",
 		title: "starscourage rhadhan",
-		price: "$2000",
+		price: "$399",
 		rating: 4.4,
 	},
 	{
 		id: "11",
 		url: "",
 		title: "phone lorem epsum dolor sit",
-		price: "$2000",
+		price: "$299",
 		rating: 4.5,
 	},
 	{
 		id: "12",
 		url: "",
 		title: "watch tres do unus",
-		price: "$2000",
+		price: "$199",
 		rating: 4,
 	},
 	{
 		id: "13",
 		url: "",
 		title: "nihil nihil nihil",
-		price: "$2000",
+		price: "$999",
 		rating: 3,
 	},
 	{
 		id: "14",
 		url: "",
 		title: "blade of miquella , malenaia",
-		price: "$2000",
+		price: "$200",
 		rating: 3.7,
 	},
 	{
 		id: "15",
 		url: "",
 		title: "starscourage rhadhan",
-		price: "$2000",
+		price: "$1235",
 		rating: 4.4,
 	},
 	{
 		id: "16",
 		url: "",
-		title: "nihil nihil nihil",
-		price: "$2000",
-		rating: 3,
+		title: "watch tres do unus",
+		price: "$199",
+		rating: 4,
 	},
 	{
 		id: "17",
 		url: "",
-		title: "blade of miquella , malenaia",
-		price: "$2000",
-		rating: 3.7,
+		title: "nihil nihil nihil",
+		price: "$999",
+		rating: 3,
 	},
 	{
 		id: "18",
 		url: "",
+		title: "blade of miquella , malenaia",
+		price: "$200",
+		rating: 3.7,
+	},
+	{
+		id: "19",
+		url: "",
 		title: "starscourage rhadhan",
-		price: "$2000",
+		price: "$1235",
 		rating: 4.4,
 	},
 ];
@@ -124,8 +139,20 @@ const itemsPerPage = 16;
 export default function ResultProducts() {
 
 	const [currentPage, setCurrentPage] = useState(1);
-
+	const [sorted, setSorted] = useState<product[]>(products);
+	const {sortBy} = useContext(MyContext)!;
     const totalPages = Math.ceil(products.length / itemsPerPage);
+	useEffect(()=>{
+		if (sortBy === 'PRICE') {
+            let sort = [...products].sort((a, b) => {
+                // Remove the $ sign and convert to number for sorting
+                const priceA = parseFloat(a.price.replace('$', ''));
+                const priceB = parseFloat(b.price.replace('$', ''));
+                return priceA - priceB; // Ascending order
+            });
+			setSorted(sort);
+        }
+	},[sortBy])
 
     const handleNextPage = () => {
         if (currentPage < totalPages) {
@@ -147,7 +174,7 @@ export default function ResultProducts() {
         setCurrentPage(totalPages);
     };
 
-    const displayedProducts = products.slice(
+    const displayedProducts = sorted.slice(
         (currentPage - 1) * itemsPerPage,
         currentPage * itemsPerPage
     );
