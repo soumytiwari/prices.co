c2d4f8dd28d8e5edc0be63c157a7b75172783a3b
Utkarsh Krishna <itsutkarshk711@gmail.com>
2024-08-25 02:47:40 +0530

    modified search bar

--- components/products/searchResult.tsx
+++ components/products/searchResult.tsx
@@ -152,6 +152,9 @@ export default function ResultProducts() {
             });
 			setSorted(sort);
         }
+		else if(sortBy === 'NONE') {
+			setSorted(products);
+		}
 	},[sortBy])
 
     const handleNextPage = () => {
