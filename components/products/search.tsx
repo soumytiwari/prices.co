e2c7cacb9ebe58e58c90ef3f97b5ca32060a7774
Utkarsh Krishna <itsutkarshk711@gmail.com>
2024-08-24 17:55:09 +0530

    modified search.tsx

--- components/products/search.tsx
+++ components/products/search.tsx
@@ -53,9 +53,9 @@ export default function SearchBar() {
 					viewBox="0 0 24 24"
 					fill="none"
 					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				>
 					<circle
 						cx="11"
