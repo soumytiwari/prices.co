6c6bb3eeb21ad2d3f3e863e2cc5c50520d0fea45
soumytiwari <tiwarisoumya111@gmail.com>
2024-11-04 12:22:35 +0530

    reference modified

--- backend/references/frnd.ts
+++ backend/references/frnd.ts
@@ -1,15 +1,15 @@
-interface Card {
+// cardInterface.ts
+export interface Card {
     id: string,
-	url: string,
-	brand?: string,
-	discount?:string,
-	productType?: string,
-	title: string,
-	price: string | number,
-	originalPrice?: string | number,
-	rating: string | number,
-	totalRating : string | number,
-	features?: {},
-	mainImage?: string,
+    url: string,
+    brand?: string,
+    discount?: string,
+    productType?: string,
+    title: string,
+    price: string | number,
+    originalPrice?: string | number,
+    rating: string | number,
+    totalRating: string | number,
+    features?: Record<string, string | number>,
+    mainImage?: string,
 }
-
