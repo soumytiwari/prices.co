2d49ae4087ecc882cfdc3ece1df522d1bd7ede93
soumytiwari <tiwarisoumya111@gmail.com>
2024-11-06 14:16:25 +0530

    created selector file

---
+++ backend/src/selectors.ts
@@ -0,0 +1,11 @@
+export const amazonSelectors = {
+    titleElement: "span.a-text-normal",
+    urlElement: "a.a-link-normal.s-no-outline",
+    brandElement: "span.a-size-base-plus",
+    mainImageElement: "img.s-image",
+    priceElement: ".a-price .a-offscreen",
+    originalPriceElement: ".a-section .a-text-price .a-offscreen",
+    ratingElement: "span.a-icon-alt",
+    totalRatingElement: "span.a-size-base.s-underline-text",
+    discountElement: "span.a-letter-space + span"
+};
